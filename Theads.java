54178ee10637e0c7a1ae2b6a84a718a51b9f6be6
pandora-silva <isadorarodrigurs@gmail.com>
2025-07-10 21:17:42 -0300

    atividade do slide - thread

--- Theads.java
+++ Theads.java
@@ -7,7 +7,7 @@ public class Theads extends Thread{
     public void run(){
     for (char letra = 'A'; letra <= 'J'; letra++){
      try{
-      Thread.sleep(1000);
+      Thread.sleep(500);
       System.out.println("Thread: " + letra);
      } catch(InterruptedException e){
         e.printStackTrace();
